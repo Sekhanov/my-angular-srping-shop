02f5bb10a5a93d85c449651b1c0838dc36cc9981
s.khanov <sekhanov@gmail.com>
2019-11-14 13:28:40 +0300

    shop-api init

---
+++ shop-api/src/main/java/com/sekhanov/shopapi/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.sekhanov.shopapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * DemoController
+ */
+@RestController
+@RequestMapping(path = "/demo")
+public class DemoController {
+    
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private static class Person {
+        private String name;
+        private int age;
+    }
+
+    @GetMapping
+    public Person getPerson() {
+        return new Person("Ivan", 22);
+    }
+}
